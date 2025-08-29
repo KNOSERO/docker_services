3760b890567837b593aceea26c1b5edb9e9ab7af
Knosero <rafalpilecki.szczecin@gmail.com>
2025-08-29 19:06:52 +0200

    init

--- script/validate_docker.sh
+++ script/validate_docker.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+has_compose_v2() { docker compose version >/dev/null 2>&1; }
+has_compose_v1() { command -v docker-compose >/dev/null 2>&1; }
+
+compose_config() {
+  local file="$1"
+  if has_compose_v2; then
+    docker compose -f "$file" config >/dev/null
+  elif has_compose_v1; then
+    docker-compose -f "$file" config -q
+  else
+    echo "ERROR: brak docker compose (ani v2, ani docker-compose v1) w PATH" >&2
+    exit 1
+  fi
+}
+
 mapfile -d '' COMPOSE_FILES < <(
   git ls-files -z | grep -zE '(^|/)(docker-)?compose([^/]*?)\.ya?ml$'
 )
